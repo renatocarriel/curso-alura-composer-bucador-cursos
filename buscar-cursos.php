73064dd14005643c74948558ccc2cc4c00057efd
Renato Casali Carriel <renato.carriel@REGISTRO.GOV>
2022-08-10 17:17:57 -0300

    Primeira versão

---
+++ buscar-cursos.php
@@ -0,0 +1,21 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new Client(['verify' => false]);
+$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/python');
+
+$html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+
+$cursos = $crawler->filter('span.card-curso__nome');
+
+foreach ($cursos as $curso) {
+    echo $curso->textContent . PHP_EOL;
+}
